8eaf379122641db3e312a8d9e04ac1bd2ce6a78f
Ahitagni Pai <89749029+ahit17@users.noreply.github.com>
2023-04-04 10:06:21 +0000

    navbar done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,16 +1,18 @@
 import "./Home.css"
-
+ 
 const Home = () => {
   return (
     <div className="desktop-1">
-    <div className="navbar">
-      <div className="logo">
-        <img src="" alt="LOGO" />
-      </div>
-      <div className="name">
-        NSEC PLACEMENT CELL
-      </div>
-    </div>
+     <nav className="navbar">
+      <img src="images/25-years-nsec-logo-3.png" alt="Logo" className="navbar__logo" />
+      <h1 className="navbar__title">NSEC PLACEMENT CELL</h1>
+      <ul className="navbar__menu">
+        <li className="navbar__menuItem"><a href="/">Home</a></li>
+        <li className="navbar__menuItem"><a href="/about">About</a></li>
+        <li className="navbar__menuItem"><a href="/contact">Contact</a></li>
+        <li className="navbar__menuItem"><a href="/login">Login</a></li>
+      </ul>
+    </nav>
     </div>
   );
 };
